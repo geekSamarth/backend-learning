e2d4ce62b8950839a126fd72f5a17c1dd093f03b
samarth goyal <samarthagrawal1804@gmail.com>
2024-03-29 01:02:20 +0530

    Jwt and user and video model

--- src/index.js
+++ src/index.js
@@ -2,7 +2,6 @@
 import  express from "express";
 import dotenv from "dotenv";
 import connectDB from "./db/index.js";
-import e from "express";
 const app = express();
 dotenv.config({ path: "./env" });
 
--- src/models/user.model.js
+++ src/models/user.model.js
@@ -52,7 +52,7 @@ const userSchema = new Schema(
 // use of pre-save middleware to hash password before saving
 
 userSchema.pre("save", async function (next) {
-  if (!this.isModified("password")) return next;
+  if (!this.isModified("password")) return next();
   this.password = bcrypt.hash(this.password, 10);
   next();
 });
